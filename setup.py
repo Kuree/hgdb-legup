69edee5628ec084849970d5ee54460f6890f2c7b
Keyi Zhang <keyi@stanford.edu>
2022-03-03 18:03:56 -0800

    start to work on inspect conversion

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+
+current_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_directory, 'README.rst')) as f:
+    long_description = f.read()
+
+
+setup(
+    name='hgdb-legup',
+    version='0.0.1',
+    author='Keyi Zhang',
+    author_email='keyi@cs.stanford.edu',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url="https://github.com/Kuree/hgdb-legup",
+    install_requires=[
+        "mysqlclient",
+    ],
+    scripts=["scripts/inspect2hgdb"],
+    python_requires=">=3.6"
+)
